e94d1cfad59ff7023211ea7412b07cefc9e63b8e
PedroHenriqueFrancozo <pedro3francozo@gmail.com>
2025-11-06 22:18:29 -0300

    upgrade

---
+++ study8.py
@@ -0,0 +1,20 @@
+# Exercício 
+nome = "Pedro"
+sobrenome = "Francozo"
+idade = 23
+ano_nascimento = 2025 - idade
+
+if idade >= 18:
+    maior_de_idade = "Sim"
+else:
+    maior_de_idade = "Não"
+    
+altura_metros = ("1,80m")
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura em metros:', altura_metros)
+
