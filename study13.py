24151c5016aa8dad3c7431a6f2b8d499b5a81931
PedroHenriqueFrancozo <pedro3francozo@gmail.com>
2025-11-07 21:09:48 -0300

    upgrade

--- study13.py
+++ study13.py
@@ -1,9 +1,17 @@
-a = 'AAAAA'
-b = 'BBBBBB'
-c = 1.1
-string = 'b={nome2} a={nome1} a={nome1} c={nome3:.2f}'
-formato = string.format(
-    nome1=a, nome2=b, nome3=c
-)
+nome = 'Luiz Otávio'
+altura = 1.80
+peso = 95
+imc = peso / altura ** 2
 
-print(formato)
+"f-strings"
+linha_1 = f'{nome} tem {altura:.2f} de altura,'
+linha_2 = f'pesa {peso} quilos e seu imc é'
+linha_3 = f'{imc:.2f}'
+
+print(linha_1)
+print(linha_2)
+print(linha_3)
+
+# Luiz Otávio tem 1.80 de altura,
+# pesa 95 quilos e seu IMC é
+# 29.320987654320987
