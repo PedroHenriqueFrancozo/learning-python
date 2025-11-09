fc3e27ecb78123d0a7b6e954f1a3763c6dc6caa8
PedroHenriqueFrancozo <pedro3francozo@gmail.com>
2025-11-09 08:24:02 -0300

    upgrade

---
+++ study20.py
@@ -0,0 +1,14 @@
+primeiro_valor = input('Digite um  valor: ')
+segundo_valor = input('Digite outro valor: ')
+
+if primeiro_valor > segundo_valor:
+    print(f'primeiro_valor= {primeiro_valor} é maior ou igual do que o segundo_valor= {segundo_valor}')
+else:
+    print(f'segundo_valor= {segundo_valor} é maior do que o primeiro_valor= {primeiro_valor}')
+
+"""
+if primeiro_valor > segundo_valor:
+    print(f'{primeiro_valor=} é maior ou igual do que {segundo_valor=}')
+else:
+    print(f'{segundo_valor=} é maior do que {primeiro_valor=s}')
+"""
