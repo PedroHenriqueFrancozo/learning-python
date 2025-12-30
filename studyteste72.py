cbc47ed40bb9b68aaa7c56be86d3cc1cd74219c3
Pedro <pedro3francozo@gmail.com>
2025-12-30 07:31:56 -0300

    update

--- studyteste72.py
+++ studyteste72.py
@@ -5,3 +5,26 @@
 # Retorne o total para uma variável e mostre o valor
 # da variável.
 
+def multiplication(*args):
+    total = 1
+    for number in args:
+            total *= number
+    return total
+
+result = multiplication(1, 2, 3, 4, 5)
+print(result)
+# print(1 * 2 * 3 * 4 * 5)
+
+# Crie uma função fala se um número é par ou impar.
+# Retorne se o número é par ou ímpar
+ 
+def pair_odd(number):
+    multiplo = number % 2 == 0 
+    
+    if multiplo:
+        return f'O número {number} é par'
+    return f'O número {number} é ímpar'
+
+print(pair_odd(10))
+
+    
