1646c85a0633b361493d82400029305d19a7bc53
Pedro <pedro3francozo@gmail.com>
2026-01-07 18:57:40 -0300

    Learning Docstrings an the print

--- 01-introduction/01-basics.py
+++ 01-introduction/01-basics.py
@@ -13,7 +13,8 @@ EN-US: This can algo be used for multi-line notes or temporary documentation.
 
 #EN-US: The # symbol is used for single-line comments.
 #EN-US: Everything after it is ignored by the Python interpreter.
-#PT-BR: Permite escrever um comentário
+#PT-BR: # Permite escrever um comentário por linha.
+#PT-BR: O interpretador do ppython ignora a linha que está o simbolo
 
 #EN-US: Basic output:
 print(123)  #EN-US: You can place commnts at the and of line
